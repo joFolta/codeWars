1367f6b8782f10f468f3574c7c610a6fb6a3644d
Johann <folta@brandeis.edu>
2020-12-07 21:27:53 -0500

    the vowel code

---
+++ codewars/6-kyu/the-vowel-code.js
@@ -0,0 +1,42 @@
+/*
+https://www.codewars.com/kata/53697be005f803751e0015aa/javascript
+
+6 kyu
+The Vowel Code
+
+Step 1: Create a function called encode() to replace all the lowercase vowels in a given string with numbers according to the following pattern:
+
+a -> 1
+e -> 2
+i -> 3
+o -> 4
+u -> 5
+For example, encode("hello") would return "h2ll4". There is no need to worry about uppercase vowels in this kata.
+
+Step 2: Now create a function called decode() to turn the numbers back into vowels according to the same pattern shown above.
+
+For example, decode("h3 th2r2") would return "hi there".
+
+For the sake of simplicity, you can assume that any numbers passed into the function will correspond to vowels.
+*/
+function encode(string) {
+  const vowelNumMap = {
+    a: 1,
+    e: 2,
+    i: 3,
+    o: 4,
+    u: 5
+  }
+  return string.split('').map(char => vowelNumMap[char] ? vowelNumMap[char] : char).join('')
+}
+
+function decode(string) {
+  const numVowelMap = {
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u'
+  }
+  return string.split('').map(char => numVowelMap[char] ? numVowelMap[char] : char).join('')
+}
