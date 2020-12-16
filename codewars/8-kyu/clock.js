1477b2524ea52199aca909573686dbc66d8e3491
Johann <folta@brandeis.edu>
2020-12-15 21:06:47 -0500

    clock

---
+++ codewars/8-kyu/clock.js
@@ -0,0 +1,18 @@
+/*
+https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a
+
+8 kyu
+Beginner Series #2 Clock
+
+Clock shows 'h' hours, 'm' minutes and 's' seconds after midnight.
+
+Your task is to make 'Past' function which returns time converted to milliseconds.
+
+Example:
+past(0, 1, 1) == 61000
+Input constraints: 0 <= h <= 23, 0 <= m <= 59, 0 <= s <= 59
+*/
+
+function past(h, m, s) {
+  return h*60*60*1000 + m*60*1000+ s*1000
+}
