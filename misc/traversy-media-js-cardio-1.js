f8af15ed8591c41409a341c65a8c29f272dd780c
Johann <folta@brandeis.edu>
2020-12-26 21:02:50 -0500

    reverseString without split, reverse, join

--- misc/traversy-media-js-cardio-1.js
+++ misc/traversy-media-js-cardio-1.js
@@ -3,7 +3,28 @@ https://youtu.be/M2bJBuaOeOQ
 */
 
 function reverseString(str) {
-  return str.split('').reverse().join('')
+  return str
+          .split('')
+          .reverse()
+          .join('')
+}
+
+function reverseString(str) {
+  const split = []
+  for(let i=0; i<str.length; i++) {
+    split.push(str[i])
+  }
+  
+  const reverse = []
+  for(let i=0; i<split.length; i++){
+    reverse.push(split[split.length -1 -i])
+  }
+  
+  let join = ''
+  for(let i=0; i<reverse.length; i++){
+    join += reverse[i]
+  }
+  return join
 }
 
 reverseString("hello")
