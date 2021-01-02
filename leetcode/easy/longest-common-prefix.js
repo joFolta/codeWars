160f95d429d9eb34edf74eaf0ee023ab9126dce6
Johann Folta <jfolta@kyruus.com>
2021-01-02 15:56:36 -0500

    TRYAGAIN longest common prefix

--- leetcode/easy/longest-common-prefix.js
+++ leetcode/easy/longest-common-prefix.js
@@ -29,6 +29,15 @@ strs[i] consists of only lower-case English letters.
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function(strs) {
-    
-};
+// https://leetcode.com/problems/longest-common-prefix/discuss/702828/Easy-JS-Solution
+// var longestCommonPrefix = function (strs) {
+//   if (!strs.length) return "";
+//   let prefix = "";
+//   for (let i = 0; i < strs[0].length; i++) {
+//     for (let j = 0; j < strs.length - 1; j++) {
+//       if (strs[j][i] !== strs[j + 1][i]) return prefix;
+//     }
+//     prefix += strs[0][i];
+//   }
+//   return prefix;
+// };
