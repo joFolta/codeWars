8232946e6444c2bed6f5014096551e094c01d0e4
Johann <folta@brandeis.edu>
2021-01-02 22:56:57 -0500

    who took the car keys

---
+++ codewars/7-kyu/who-took-car-key.js
@@ -0,0 +1,19 @@
+/*
+https://www.codewars.com/kata/57a23c2acf1fa514d0001034
+
+7 kyu
+Who Took The Car Key ?
+
+You woke up from a massive headache and can't seem to find your car key. You find a note with a clue that says:
+
+*"If you're reading this then I have left the state and am well on my way to freedom. Just to make things interesting, I have also provided something for you to track me with. Let the chase begin..."*
+
+Given an array of binary numbers, figure out and return the culprit's message to find out who took the missing car key.
+
+> whoTookTheCarKey(['01000001', '01101100', '01100101', '01111000', '01100001', '01101110', '01100100', '01100101', '01110010'])
+'Alexander'
+*/
+
+whoTookTheCarKey = function(message) {
+  return message.map(binary => String.fromCharCode(parseInt(binary,2))).join('')
+}
