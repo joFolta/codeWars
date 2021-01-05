4a9204888deb95c0840d0cb05e1620980eaf08e8
Johann <folta@brandeis.edu>
2021-01-04 21:08:32 -0500

    max diff easy

---
+++ codewars/7-kyu/max-diff-easy.js
@@ -0,0 +1,31 @@
+/*
+https://www.codewars.com/kata/588a3c3ef0fbc9c8e1000095
+
+7 kyu
+max diff - easy
+
+You must implement a function that return the difference between the biggest and the smallest value in a list(lst) received as parameter.
+
+The list(lst) contains integers, that means it may contain some negative numbers.
+
+If the list is empty or contains a single element, return 0.
+
+The list(lst) is not sorted.
+
+maxDiff([1, 2, 3, 4]); // return 3, because 4 - 1 == 3
+maxDiff([1, 2, 3, -4]); // return 7, because 3 - (-4) == 7
+Have fun!
+*/
+
+function maxDiff(list) {
+  if (!list.length) return 0
+  const sortedList = list.sort((a,b) => a-b)
+  const max = list[sortedList.length - 1]
+  const min = list[0]
+  return max - min
+};
+
+function maxDiff(list) {
+  if (!list.length) return 0
+  return Math.max(...list) - Math.min(...list)
+};
