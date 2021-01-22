ade3b9a283b2603bdac2a7567031f9eb3348de71
Johann <folta@brandeis.edu>
2021-01-22 17:45:46 -0500

    shortest word

---
+++ codewars/7-kyu/shortest-word.js
@@ -0,0 +1,31 @@
+/* 
+https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9
+
+7 kyu
+Shortest Word
+
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+*/
+
+function findShort(s){
+  const words = s.split(' ')
+  let shortest = words[0]
+  words.forEach(word => {
+    if (word.length < shortest.length) {
+      shortest = word
+    }
+  })
+  return shortest.length
+}
+
+function findShort(s) {
+  const words = s.split(' ')
+  let shortest = words[0]
+  words.forEach(word => {
+    word.length < shortest.length ?
+      shortest = word : null
+  })
+  return shortest.length
+}
