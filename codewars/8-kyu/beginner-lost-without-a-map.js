d6918097eed39293158902b899af5894cc097915
Johann <folta@brandeis.edu>
2021-04-04 23:33:56 -0400

    beginner lost without a map

---
+++ codewars/8-kyu/beginner-lost-without-a-map.js
@@ -0,0 +1,18 @@
+/* 
+https://www.codewars.com/kata/57f781872e3d8ca2a000007e
+
+8 kyu
+Beginner - Lost Without a Map
+
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+
+For the beginner, try to use the map method - it comes in very handy quite a lot so is a good one to know.
+*/
+
+function maps(x) {
+  return x.map(num => num*2)
+}
