4a8937c1e1f0c918dcd838af05ca3e2e9eb3629d
Johann <folta@brandeis.edu>
2021-04-07 22:50:48 -0400

    Coding Meetup #6 - Higher-Order Functions Series - Can they code in the same language?

---
+++ codewars/7-kyu/coding-meetup-6-they-code-same-language.js
@@ -0,0 +1,31 @@
+/* 
+https://www.codewars.com/kata/coding-meetup-number-6-higher-order-functions-series-can-they-code-in-the-same-language
+
+7 kyu
+Coding Meetup #6 - Higher-Order Functions Series - Can they code in the same language?
+
+You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return either:
+
+true if all developers in the list code in the same language; or
+false otherwise.
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'JavaScript' },
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'JavaScript' },
+  { firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 65, language: 'JavaScript' },
+];
+your function should return true.
+
+Notes:
+
+The strings representing a given language will always be formatted in the same way (e.g. 'JavaScript' will always be formatted will upper-case 'J' and 'S'
+The input array will always be valid and formatted as in the example above.
+*/
+
+function isSameLanguage(list) {
+  const lang = list[0].language
+  return list.every(({language}) => lang === language)
+}
