f0fe3fefae7b7f69bcfb4f0e841b0ca653e40afc
Johann <folta@brandeis.edu>
2021-04-10 23:57:16 -0400

    Improved solution, Coding Meetup #9 - Higher-Order Functions Series - Is the meetup age-diverse?

--- codewars/6-kyu/coding-meetup-9-is-meetup-age-diverse.js
+++ codewars/6-kyu/coding-meetup-9-is-meetup-age-diverse.js
@@ -57,3 +57,8 @@ function isAgeDiverse(list) {
   
   return false
 }
+
+// better answer
+function isAgeDiverse(list) {
+  return [[12,20],[20,30],[30,40],[40,50],[50,60],[60,70],[70,80],[80,90],[90,100],[100,200]].every(([minAge,maxAge]) => list.some(({age}) => age >= minAge && age < maxAge))
+}
