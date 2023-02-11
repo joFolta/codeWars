4ec6496f628bcf1dac6c5cca946874202c7c7540
Johann <folta@brandeis.edu>
2023-02-11 07:10:56 -0500

    Opposite Number

---
+++ codewars/8-kyu/opposite-number.js
@@ -0,0 +1,15 @@
+/* https://www.codewars.com/kata/56dec885c54a926dcd001095/train/javascript
+
+8 kyu
+Opposite Number
+
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+*/
+
+const opposite = (number) => number * -1;
