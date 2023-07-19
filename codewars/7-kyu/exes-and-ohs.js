306c624e3b9c43dd453989662ff6a382f733d178
Johann <folta@brandeis.edu>
2023-07-19 14:10:22 -0400

    Exes and Ohs

---
+++ codewars/7-kyu/exes-and-ohs.js
@@ -0,0 +1,38 @@
+/*
+https://www.codewars.com/kata/55908aad6620c066bc00002a
+
+7 kyu
+Exes and Ohs
+
+Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+Examples input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false
+*/
+
+function XO(str) {
+  let x = 0;
+  let o = 0;
+
+  str.split("").forEach((char) => {
+    if (char === "x" || char === "X") {
+      x++;
+    }
+    if (char === "o" || char === "O") {
+      o++;
+    }
+  });
+
+  return x - o === 0 ? true : false;
+}
+
+function XO(str) {
+  const xs = str.match(/[xX]/g) || [];
+  const os = str.match(/[oO]/g) || [];
+  return xs.length === os.length;
+}
