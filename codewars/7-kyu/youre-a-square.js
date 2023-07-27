621a34a4e3c1fff9022ede7fdeb673bb6c6a3ad0
Johann <folta@brandeis.edu>
2023-07-27 12:52:14 -0400

    You're a square!

---
+++ codewars/7-kyu/youre-a-square.js
@@ -0,0 +1,33 @@
+/*
+https://www.codewars.com/kata/54c27a33fb7da0db0100040e
+
+7 kyu
+You're a square!
+
+A square of squares
+You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
+
+However, sometimes, you can't arrange them into a square. Instead, you end up with an ordinary rectangle! Those blasted things! If you just had a way to know, whether you're currently working in vain… Wait! That's it! You just have to check if your number of building blocks is a perfect square.
+
+Task
+Given an integral number, determine if it's a square number:
+
+In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+
+The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
+Examples
+-1  =>  false
+ 0  =>  true
+ 3  =>  false
+ 4  =>  true
+25  =>  true
+26  =>  false
+*/
+
+// time complexity: O(1) - constant time - time does not increase with input size
+// space complexity: O(1) - constant space - memory does not increase with input size
+const isSquare = n => {
+  const squareRoot = Math.sqrt(n)
+  return squareRoot === Math.trunc(squareRoot)
+}
