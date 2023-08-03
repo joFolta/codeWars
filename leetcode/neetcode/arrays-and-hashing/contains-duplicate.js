efae4fb209311fac60ac89de09f353e803c546ff
Johann <folta@brandeis.edu>
2023-08-03 14:00:04 -0400

    Neetcode - Contains Duplicate: Linear Search, HeapSort, Hash Set, Hash Set - Early Exit

--- leetcode/neetcode/arrays-and-hashing/contains-duplicate.js
+++ leetcode/neetcode/arrays-and-hashing/contains-duplicate.js
@@ -28,23 +28,77 @@ Constraints:
 -109 <= nums[i] <= 109
 */
 
-// time complexity: O(n^2) - quadratic time due to nested loops (BAD)
-// space complexity: O(1) - constant space
 /**
+ * Brute Force - Linear Search
+ * Time complexity: O(N^2) - quadratric time - BAD
+ * Space complexity: O(1) constant space
  * @param {number[]} nums
  * @return {boolean}
  */
 var containsDuplicate = function (nums) {
-  let current;
-  let another;
-  for (let i = 0; i < nums.length; i++) {
-    current = nums[i];
-    for (let j = i + 1; j < nums.length; j++) {
-      another = nums[j];
-      if (current === another) {
-        return true;
-      }
+  for (let right = 0; right < nums.length; right++) {
+    // Time O(N)
+    for (let left = 0; left < right; left++) {
+      // Time O(N)
+      const isDuplicate = nums[left] === nums[right];
+      if (isDuplicate) return true;
     }
+  }
+
+  return false;
+};
+
+/**
+ * Sort - HeapSort
+ * Time complexity: O(N * log(N)) (sorting)
+ * Space complexity: O(1)
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = (nums) => {
+  nums.sort((a, b) => a - b); // Time O(N * log(N)) | Space O(1))
+
+  return hasDuplicate(nums);
+};
+
+const hasDuplicate = (nums) => {
+  for (let curr = 0; curr < nums.length - 1; curr++) {
+    // Time O(N)
+    const next = curr + 1;
+
+    const isNextDuplicate = nums[curr] === nums[next];
+    if (isNextDuplicate) return true;
+  }
+
+  return false;
+};
+
+/** Hash Set
+ * Time complexity: O(N) - linear time with Hash Set (~ Hash Table?)
+ * Space complexity: O(N) - linear space - memory usage correlates to input size
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = (nums) => {
+  const numSet = new Set(nums); // Time O(N) | Space O(N)
+  const isEqual = numSet.size === nums.length;
+
+  return !isEqual;
+};
+
+/** Hash Set - Early Exit
+ * Time complexity: O(N)
+ * Space complexity: O(N)
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = (nums) => {
+  const newSet = new Set();
+  for (const num of nums) {
+    // Time O(N)
+    if (newSet.has(num)) return true;
+
+    newSet.add(num); // Space O(N)
   }
 
   return false;
