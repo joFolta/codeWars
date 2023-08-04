f89f1b13b4ef9b078620c27579bf648ea6f6c458
Johann <folta@brandeis.edu>
2023-08-04 12:41:07 -0400

    Neetcode - Valid Anagram: HeapSort, HashMap

--- leetcode/neetcode/arrays-and-hashing/valid-anagram.js
+++ leetcode/neetcode/arrays-and-hashing/valid-anagram.js
@@ -31,15 +31,15 @@ Follow up: What if the inputs contain Unicode characters? How would you adapt yo
 
 /**
  * Brute Force
- * Time complexity: O(N) - linear time
- * Space complexity: O(N) - linear space
+ * Time complexity: O(N * log(N)) - loglinear complexity
+ * Space complexity: O(1) - constant space
  * @param {string} s
  * @param {string} t
  * @return {boolean}
  */
 var isAnagram = function (s, t) {
-  const sArr = s.split("").sort(); // Time O(N) | Space O(N)
-  const tArr = t.split("").sort();
+  const sArr = sortAsArr(s);
+  const tArr = sortAsArr(t);
   const longestArrLength =
     sArr.length > tArr.length ? sArr.length : tArr.length;
 
@@ -51,10 +51,15 @@ var isAnagram = function (s, t) {
   return true;
 };
 
+const sortAsArr = (string) =>
+  string
+    .split("") // Time O(N) | Space O(N)
+    .sort(); // Time O(N * log(N)) | Space O(1)
+
 /**
- * Object Mapping
+ * Hash Map - Object Mapping
  * Time complexity: O(N) linear time
- * Space complexity: O(N) linear space
+ * Space complexity: O(N) linear space ??? constant space O(1)? (see hashmap example at bottom)
  * @param {string} s
  * @param {string} t
  * @return {boolean}
@@ -81,4 +86,78 @@ var isAnagram = function (s, t) {
   }
 
   return Object.values(mapStore).every((val) => val === 0); // Time O(N)
+};
+
+// ----------------------------------------------------
+// ----------------Neetcode Solutions------------------
+// ----------------------------------------------------
+
+/**
+ * Sort - HeapSort Space O(1) | Quicksort Space O(log(N))
+ * Time complexity: O(N * log(N)) (worse than O(N))
+ * Space complexity: O(N) - linear space
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  const isEqual = s.length === t.length;
+  if (!isEqual) return false;
+
+  return reorder(t) === reorder(s); // Time O(N * log(N)) | Space O(N)
+};
+
+const reorder = (str) =>
+  str
+    .split("") // Time O(N) | Space O(N)
+    .sort((a, b) => a.localeCompare(b)) // Time O(N * log(N)) | Space O(1) | O(log(N))
+    .join(""); // Time O(N) | Space O(N)
+
+/**
+ * Hash Map - Frequency Counter
+ * Time complexity: O(N) - linear time
+ * Space complexity: O(1) - constant time
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  const map = new Map();
+  const isEqual = s.length === t.length;
+  if (!isEqual) return false;
+
+  addFrequency(s, map); // Time O(N) | Space O(1)
+  subtractFrequency(t, map); // Time O(N) | Space O(1)
+
+  return checkFrequency(map); // Time O(N)
+};
+
+const addFrequency = (str, map) => {
+  for (const char of str) {
+    // Time O(N)
+    const count = (map.get(char) || 0) + 1;
+
+    map.set(char, count); // Space O(1)
+  }
+};
+
+const subtractFrequency = (str, map) => {
+  for (const char of str) {
+    // Time O(N)
+    if (!map.has(char)) continue; // continue - terminated current iteration and continue to next iteration
+
+    const count = map.get(char) - 1;
+
+    map.set(char, count); // Space O(1)
+  }
+};
+
+const checkFrequency = (map) => {
+  for (const [char, count] of map) {
+    // Time O(N)
+    const isEmpty = count === 0;
+    if (!isEmpty) return false;
+  }
+
+  return true;
 };
